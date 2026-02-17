35c36554923497e93ad0965c5fbcd31d5c58f897
Tan Jun Yan <yan@yan3321.com>
2026-02-17 10:38:22 +0800

    refactor(hooks): optimize uploadToRobloxHook logic and improve readability

--- src/hooks/mediaRoblox.ts
+++ src/hooks/mediaRoblox.ts
@@ -1,16 +1,16 @@
-import uploadToRoblox from '@/functions/uploadToRoblox'
-import { Media } from '@/payload-types'
 import type { CollectionBeforeChangeHook } from 'payload'
+import { Media } from '@/payload-types'
+import uploadToRoblox from '@/functions/uploadToRoblox'
 
 export const uploadToRobloxHook: CollectionBeforeChangeHook<Media> = async ({
+  originalDoc,
   data,
   operation,
   req,
 }) => {
-  if (operation === 'create') {
+  if (operation === 'create' || originalDoc?.filesize !== data?.filesize) {
     try {
-      const robloxAssetId = await uploadToRoblox(req.file)
-      data.robloxAssetId = robloxAssetId
+      data.robloxAssetId = await uploadToRoblox(req.file)
     } catch (error) {
       console.error('Error uploading to Roblox:', error)
       // Optionally handle the error further, e.g., update a status field or notify admin
