346b4f1a2e4f24120244e35540292e32128f005e
Tan Jun Yan <yan@yan3321.com>
2026-02-28 12:46:16 +0800

    build(deps): bump deps

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -100,6 +100,9 @@ export interface Config {
     'event-countdown': EventCountdownSelect<false> | EventCountdownSelect<true>;
   };
   locale: null;
+  widgets: {
+    collections: CollectionsWidget;
+  };
   user: User;
   jobs: {
     tasks: unknown;
@@ -431,6 +434,16 @@ export interface EventCountdownSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
   globalType?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "collections_widget".
+ */
+export interface CollectionsWidget {
+  data?: {
+    [k: string]: unknown;
+  };
+  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
