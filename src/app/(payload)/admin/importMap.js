771bf503a776a103d197fe40843f9ba33c93253f
Tan Jun Yan <yan@yan3321.com>
2026-06-07 11:17:34 +0800

    build(deps): bump deps

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -3,6 +3,7 @@ import { default as default_b9735679988ce1ed6e99d76e888e6f17 } from 'src/graphic
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
+/** @type import('payload').ImportMap */
 export const importMap = {
   "src/graphics/Icon#default": default_4585126b52d88fbb7656ca756c90ad9f,
   "src/graphics/Logo#default": default_b9735679988ce1ed6e99d76e888e6f17,
